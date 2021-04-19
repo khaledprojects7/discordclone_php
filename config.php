632e5985b32b58caa22a4eee6a3e71f09aac914f
loukmanekhaled1 <loukmanekhaled.education@gmail.com>
2021-04-19 14:38:11 +0000

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 
 try{
-    $db = new PDO("mysql:host=bvnjezp1zbqgentz1qpy-mysql.services.clever-cloud.com;dbname=bvnjezp1zbqgentz1qpy;","u1ntbjeslurlseig","GptO9be7GfCJM2PcEcbv");
+    $db = new PDO("mysql:host=bvnjezp1zbqgentz1qpy-mysql.services.clever-cloud.com;dbname=bvnjezp1zbqgentz1qpy;charset=utf8mb4","u1ntbjeslurlseig","GptO9be7GfCJM2PcEcbv");
     $db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 }catch (PDOException $e)
 {
